5b1cfa4938cff1e0acfb7a67b098a71b689e4e17
nathan-m-schneider-22 <nathan.m.schneider.22@dartmouth.edu>
2022-02-21 23:20:18 -0500

    engs21 placeholder

--- src/components/app.js
+++ src/components/app.js
@@ -8,6 +8,7 @@ import Bio from './bio';
 import Projects from './projects';
 import Questions from './questions';
 import Facts from './facts';
+import ENGS21 from './engs21';
 
 const Nav = () => {
   return (
@@ -24,7 +25,7 @@ const Privacy = () => {
   return (
     <div className="privacy">
       <p>
-        By visiting this site you agree to have your attendence logged and reported to me.
+        By visiting this site you agree to have your attendance logged and reported to me.
         For more information see <a href="https://github.com/nathan-m-schneider-22/personal-site">the repository</a>
       </p>
     </div>
@@ -44,6 +45,8 @@ const App = (props) => {
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/questions" component={Questions} />
           <Route exact path="/facts" component={Facts} />
+          <Route exact path="/engs21" component={ENGS21} />
+
           <Route component={FallBack} />
         </Switch>
         <Privacy />
---
+++ src/components/engs21.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/questions.scss';
+
+class ENGS21 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="facts-container">
+        <div>
+          <h1>ENGS 021 Portfolio</h1>
+          <h3>Linked in the Canvas assignment, I'll do this when I have time</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ENGS21;
