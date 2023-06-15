09e0743aa26d0d581c64a7d64bd42ebed5c3cb62
nathan-m-schneider-22 <nathan.m.schneider.22@dartmouth.edu>
2023-06-15 00:12:58 -0700

    fixing favicon

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(jpe?g|png|gif|svg|pdf)$/,
+        test: /\.(jpe?g|png|gif|svg|pdf|ico)$/,
         use: [
           {
             loader: 'file-loader',
